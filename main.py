1f4e7aa88b3ea6693768695df647b1ada1fbac28
ndass6 <ND@USComputers.net>
2017-03-11 15:49:40 -0500

    Fixed database info

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 app.secret_key = 'D8K27qBS8{8*sYVU>3DA530!0469x{'
 
 mysql = MySQL()
-app.config['MYSQL_DATABASE_USER'] = 'ndass$default'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'ndass'
-app.config['MYSQL_DATABASE_DB'] = 'MUCproject123'
+app.config['MYSQL_DATABASE_USER'] = 'ndass'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'MUCproject123'
+app.config['MYSQL_DATABASE_DB'] = 'ndass$default'
 app.config['MYSQL_DATABASE_HOST'] = 'ndass.mysql.pythonanywhere-services.com'
 mysql.init_app(app)
 
