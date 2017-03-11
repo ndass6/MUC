3fef1138b207ce99e5d0c301a6cd35c28d0f08f1
ndass6 <ND@USComputers.net>
2017-03-11 14:28:41 -0500

    Added admin sending messages

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, request
 from flaskext.mysql import MySQL
 
 app = Flask(__name__)
@@ -10,12 +10,42 @@ app.config['MYSQL_DATABASE_DB'] = 'sql9163335'
 app.config['MYSQL_DATABASE_HOST'] = 'sql9.freemysqlhosting.net'
 mysql.init_app(app)
 
-cursor = mysql.connect().cursor()
+db = mysql.connect()
+cursor = db.cursor()
 
 @app.route("/")
-def hello():
-    cursor.execute("SELECT `message` FROM `users`")
-    return render_template("login.html", input=cursor.fetchone()[0])
+def login():
+    return render_template("login.html")
+
+@app.route('/processLogin', methods=['GET', 'POST'])
+def processLogin():
+    username = request.form['username']
+    password = request.form['password']
+    cursor.execute("SELECT `type`, `message` FROM `users` WHERE `username`=%s AND `password`=%s", [username, password])
+    user_data = cursor.fetchone()
+    if user_data[0] == 'admin':
+        cursor.execute("SELECT `username` FROM `users` WHERE `type`='user'")
+        usernames = cursor.fetchone()
+        return render_template('admin.html', usernames=usernames)
+    else:
+        return user_data[1]
+
+@app.route('/sendMessage', methods=['GET', 'POST'])
+def sendMessage():
+    username = request.form['username']
+    message = request.form['message']
+    print(username, message)
+    cursor.execute("UPDATE `users` SET `message`=%s WHERE `username`=%s", [message, username])
+    db.commit()
+
+    cursor.execute("SELECT `username`, `message` FROM `users` WHERE `username`=username")
+    data = cursor.fetchone()
+    print(data)
+
+    cursor.execute("SELECT `username` FROM `users` WHERE `type`='user'")
+    usernames = cursor.fetchone()
+    return render_template('admin.html', usernames=usernames)
+
 
 if __name__ == "__main__":
     app.run()
