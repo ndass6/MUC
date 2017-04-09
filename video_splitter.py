2abd4535365e10235783f735264cdeb237bbf599
ndass6 <ND@USComputers.net>
2017-04-09 12:41:36 -0400

    Added video_splitter

---
+++ video_splitter.py
@@ -0,0 +1,29 @@
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+import os
+
+FILE_PATH = "Videos/Experiment 7/"
+VIDEO_NAME = "a.mp4"
+ffmpeg_extract_subclip(FILE_PATH + VIDEO_NAME, 0 , 33, targetname = (FILE_PATH + "Clips/Clip 01.mp4"))
+ffmpeg_extract_subclip(FILE_PATH + VIDEO_NAME, 34, 70, targetname = (FILE_PATH + "Clips/Clip 02.mp4"))
+ffmpeg_extract_subclip(FILE_PATH + VIDEO_NAME, 34, 70, targetname = (FILE_PATH + "Clips/Clip 03.mp4"))
+ffmpeg_extract_subclip(FILE_PATH + VIDEO_NAME, 34, 70, targetname = (FILE_PATH + "Clips/Clip 04.mp4"))
+ffmpeg_extract_subclip(FILE_PATH + VIDEO_NAME, 34, 70, targetname = (FILE_PATH + "Clips/Clip 05.mp4"))
+ffmpeg_extract_subclip(FILE_PATH + VIDEO_NAME, 34, 70, targetname = (FILE_PATH + "Clips/Clip 06.mp4"))
+ffmpeg_extract_subclip(FILE_PATH + VIDEO_NAME, 34, 70, targetname = (FILE_PATH + "Clips/Clip 07.mp4"))
+ffmpeg_extract_subclip(FILE_PATH + VIDEO_NAME, 34, 70, targetname = (FILE_PATH + "Clips/Clip 08.mp4"))
+ffmpeg_extract_subclip(FILE_PATH + VIDEO_NAME, 34, 70, targetname = (FILE_PATH + "Clips/Clip 09.mp4"))
+ffmpeg_extract_subclip(FILE_PATH + VIDEO_NAME, 34, 70, targetname = (FILE_PATH + "Clips/Clip 10.mp4"))
+ffmpeg_extract_subclip(FILE_PATH + VIDEO_NAME, 34, 70, targetname = (FILE_PATH + "Clips/Clip 11.mp4"))
+ffmpeg_extract_subclip(FILE_PATH + VIDEO_NAME, 34, 70, targetname = (FILE_PATH + "Clips/Clip 12.mp4"))
+ffmpeg_extract_subclip(FILE_PATH + VIDEO_NAME, 34, 70, targetname = (FILE_PATH + "Clips/Clip 13.mp4"))
+ffmpeg_extract_subclip(FILE_PATH + VIDEO_NAME, 34, 70, targetname = (FILE_PATH + "Clips/Clip 14.mp4"))
+ffmpeg_extract_subclip(FILE_PATH + VIDEO_NAME, 34, 70, targetname = (FILE_PATH + "Clips/Clip 15.mp4"))
+ffmpeg_extract_subclip(FILE_PATH + VIDEO_NAME, 34, 70, targetname = (FILE_PATH + "Clips/Clip 16.mp4"))
+ffmpeg_extract_subclip(FILE_PATH + VIDEO_NAME, 34, 70, targetname = (FILE_PATH + "Clips/Clip 17.mp4"))
+ffmpeg_extract_subclip(FILE_PATH + VIDEO_NAME, 34, 70, targetname = (FILE_PATH + "Clips/Clip 18.mp4"))
+ffmpeg_extract_subclip(FILE_PATH + VIDEO_NAME, 34, 70, targetname = (FILE_PATH + "Clips/Clip 19.mp4"))
+ffmpeg_extract_subclip(FILE_PATH + VIDEO_NAME, 34, 70, targetname = (FILE_PATH + "Clips/Clip 20.mp4"))
+ffmpeg_extract_subclip(FILE_PATH + VIDEO_NAME, 34, 70, targetname = (FILE_PATH + "Clips/Clip 21.mp4"))
+ffmpeg_extract_subclip(FILE_PATH + VIDEO_NAME, 34, 70, targetname = (FILE_PATH + "Clips/Clip 22.mp4"))
+ffmpeg_extract_subclip(FILE_PATH + VIDEO_NAME, 34, 70, targetname = (FILE_PATH + "Clips/Clip 23.mp4"))
+ffmpeg_extract_subclip(FILE_PATH + VIDEO_NAME, 34, 70, targetname = (FILE_PATH + "Clips/Clip 24.mp4"))
