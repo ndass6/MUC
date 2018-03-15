c5072c269060b4da6ad6da1b8b5bbdb120e20fe3
ndass <ndass6@gatech.edu>
2018-03-14 23:50:59 -0700

    Changed experiment back to normal one

--- main.py
+++ main.py
@@ -306,18 +306,18 @@ def processAdmin():
     elif 'Survey' in request.form:
         return redirect('/survey')
     else:
-        # return redirect('/startExperiment')
-        return redirect('/startNewExperiment')
+        return redirect('/startExperiment')
+        # return redirect('/startNewExperiment')
 
 @app.route('/survey')
 def survey():
-    get_cursor().execute("SELECT `experiment` FROM `experiments_40deg`")
+    get_cursor().execute("SELECT `experiment` FROM `experiments_knows`")
     experiments = get_cursor().fetchall()
     return render_template('survey.html', messageTexts=messageTexts, experiments=experiments)
 
 @app.route('/processSurvey', methods=['GET', 'POST'])
 def processSurvey():
-    get_cursor().execute("""INSERT INTO `surveys_40deg`(`experiment`,`1`,`2`,`3`,`4`,`5`,`6`,`7`,`8`,`9`,
+    get_cursor().execute("""INSERT INTO `surveys_knows`(`experiment`,`1`,`2`,`3`,`4`,`5`,`6`,`7`,`8`,`9`,
         `10`,`11`,`12`,`13`,`14`,`15`,`16`,`17`,`18`,`19`,`20`,`21`,`22`,`23`,`24`) VALUES (%s,
         %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""",
         [request.form['experiment'],request.form['1'],request.form['2'],request.form['3'],request.form['4'],
@@ -331,11 +331,11 @@ def processSurvey():
 @app.route('/results')
 def results():
     results = { 'No message (raw)' : {}, '20 degrees' : {}, '30 degrees' : {}, '40 degrees' : {} }
-    get_cursor().execute("SELECT * FROM `surveys_5s`")
+    get_cursor().execute("SELECT * FROM `surveys_knows`")
     rawData = get_cursor().fetchall()
     for rawResults in rawData:
         experiment = rawResults[0]
-        get_cursor().execute("SELECT `order` FROM `experiments_5s` WHERE `experiment`=%s", [experiment])
+        get_cursor().execute("SELECT `order` FROM `experiments_knows` WHERE `experiment`=%s", [experiment])
         order = get_cursor().fetchone()[0]
         num = 0
 
@@ -385,7 +385,7 @@ def results():
 
 @app.route('/startExperiment')
 def startExperiment():
-    get_cursor().execute("SELECT `experiment`, `order` FROM `experiments_5s`")
+    get_cursor().execute("SELECT `experiment`, `order` FROM `experiments_knows`")
     data = [[x[0] + 1, x[1] + 1] for x in get_cursor().fetchall()]
     next = max(data)
     next[1] = 12 if next[1] % 12 == 0 else next[1] % 12
@@ -478,7 +478,7 @@ def experiment():
             session['clip'] = 1
             file.close()
 
-            get_cursor().execute("INSERT INTO `experiments_5s`(`experiment`,`order`) VALUES (%s,%s)",
+            get_cursor().execute("INSERT INTO `experiments_knows`(`experiment`,`order`) VALUES (%s,%s)",
                 [session['nextNum'], session['order'].split(' ')[1]])
             get_db().commit()
 
