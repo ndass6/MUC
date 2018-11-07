638448c1abd6e9e87a3f43d797635244997fdf96
Anna K. Klaussen <anna.klaussen@gmail.com>
2018-11-07 17:49:37 -0500

    Update video_splitter to work on MacOS

--- video_splitter.py
+++ video_splitter.py
@@ -2,16 +2,20 @@ import imageio
 imageio.plugins.ffmpeg.download()
 
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
-import easygui, sys, os
+import easygui, sys, os, subprocess
 
 
 if not easygui.ccbox("Select the full video."):
     sys.exit(0)
-videoPath = easygui.fileopenbox()
+videoPath = easygui.fileopenbox(filetypes=['*.mp4'])
 
 if not easygui.ccbox('Determine the time in the full video when the "Start Experiment" button was pressed.'):
     sys.exit(0)
-os.startfile(videoPath)
+if sys.platform == "win32":
+    os.startfile(videoPath)
+else:
+    opener ="open" if sys.platform == "darwin" else "xdg-open"
+    subprocess.call([opener, videoPath])
 
 offset = easygui.integerbox('Type in the time in the full video when the "Start Experiment" button was pressed.')
 if not offset:
@@ -23,7 +27,7 @@ clipPath = easygui.diropenbox()
 
 if not easygui.ccbox("Select the log file."):
     sys.exit(0)
-logPath = easygui.fileopenbox()
+logPath = easygui.fileopenbox(filetypes=['*.txt'])
 
 logFile = open(logPath, 'r')
 times = [int(line.split(' ')[0]) for line in logFile]
